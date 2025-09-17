5ebd43786c4f1ed11e90fdb0f9ace0355634c684
Maximilian <scientificresearch309@gmail.com>
2025-09-18 00:25:25 +0200

    message in store.tsx works

--- src/store.tsx
+++ src/store.tsx
@@ -1,13 +1,11 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
 interface IStore {
   message: string;
   setMessage: (message: string) => void;
 }
 
-export const useStore = create<IStore>(
-  (set): IStore => ({
-    message: 'test',
-    setMessage: (message: string) => set((state) => ({ ...state, message })),
-  })
-);
+export const useStore = create<IStore>((set) => ({
+  message: 'test in Zustand',
+  setMessage: (message) => set((state) => ({ ...state, message })),
+}));
