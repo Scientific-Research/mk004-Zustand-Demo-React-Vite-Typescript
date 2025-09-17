dc5725e632d9339b59bf15cb70b827691d9112d2
Maximilian <scientificresearch309@gmail.com>
2025-09-17 23:27:51 +0200

    add some more features to InfoBox.tsx

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -6,7 +6,10 @@ export const InfoBox = ({ message }: IMessage) => {
   return (
     <div className="infoBox">
       <h2>Another component:</h2>
-      <p>message:{message}</p>
+      <div className="row">
+        <label htmlFor="">Message:</label> {''}
+        <span className="theValue">{message}</span>
+      </div>
     </div>
   );
 };
