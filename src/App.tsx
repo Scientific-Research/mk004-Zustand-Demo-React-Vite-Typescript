5595d3ee6ae08b848e6e2d281812a5987fbe0444
Maximilian <scientificresearch309@gmail.com>
2025-09-17 20:34:24 +0200

    showing the message using state varibale

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 // import './App.css'
 
+import { useState } from 'react';
+
 function App() {
+  const [message, setMessage] = useState('test');
+
   return (
     <div className="App">
       <h1>Zustand-Demo-React-Vite-Typescript</h1>
       <p>Welcome to this Site!</p>
+      <p>Message: {message}</p>
     </div>
   );
 }
