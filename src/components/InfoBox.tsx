77da63f3f6ff9c0dcc1a73716b07480620a346ab
Maximilian <scientificresearch309@gmail.com>
2025-09-18 18:52:05 +0200

    the colors section added to the InfoBox.tsx

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -13,10 +13,20 @@ export const InfoBox = () => {
   return (
     <div className="infoBox">
       <h2>Another component:</h2>
+
+      {/* MESSAGE */}
       <div className="row">
         <label htmlFor="">Message:</label> {''}
         <span className="theValue">{message}</span>
         {/* <span className="theValue">{store.message}</span> */}
+      </div>
+
+      {/* COLORS */}
+      <div className="row">
+        <label>Colors:</label>{' '}
+        <span className="theValue">
+          {store.colors.map((m) => m).join(', ')}
+        </span>
       </div>
     </div>
   );
