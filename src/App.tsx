105566d797bfec71555d6a4034b7bd239aac6940
Maximilian <scientificresearch309@gmail.com>
2025-09-18 11:23:06 +0200

    Doing the same but with store and useStore function in Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 // import './App.css'
-
-import { useState } from 'react';
+// import { useState } from 'react';
 import { InfoBox } from './components/InfoBox';
+import { useStore } from './store';
 
 function App() {
-  const [message, setMessage] = useState('test');
+  // const [message, setMessage] = useState('test');
+  const store = useStore((state) => state);
+  const { message, setMessage } = store; // Deconstructuring => Don't need to repeat the store!
 
   const handleDisplayMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
+    // store.setMessage(e.target.value);
   };
 
   return (
@@ -21,16 +24,19 @@ function App() {
             <input
               type="text"
               value={message}
+              // value={store.message}
               onChange={(e) => handleDisplayMessage(e)}
             />
             <p>
               Message: <span>{message}</span>
+              {/* Message: <span>{store.message}</span> */}
             </p>
           </div>
         </section>
 
         <section className="dataArea">
           <InfoBox message={message}></InfoBox>
+          {/* <InfoBox message={store.message}></InfoBox> */}
         </section>
       </main>
     </div>
